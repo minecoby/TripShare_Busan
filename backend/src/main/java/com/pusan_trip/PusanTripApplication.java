e7ebda6744f547869a7442912a71efdb3ac30f96
minecoby <minecoby@naver.com>
2025-05-11 20:58:02 +0900

    chore: 백엔드 spring boot 빌드 기초 파일 설정
    백엔드 spring boot 빌드 기초 파일 설정

---
+++ backend/src/main/java/com/pusan_trip/PusanTripApplication.java
@@ -0,0 +1,13 @@
+package com.pusan_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PusanTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PusanTripApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pusan_trip/PusanTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pusan_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PusanTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
