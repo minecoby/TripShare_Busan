b2b51644b7c5450cdd59a832654ec6c5e84d4387
hyeeun <silver807@pusan.ac.kr>
2025-05-29 15:39:49 +0900

    feat: 로그인 api 일부 구현

--- backend/src/main/java/com/pusan_trip/controller/UserController.java
+++ backend/src/main/java/com/pusan_trip/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.pusan_trip.controller;
 
+import com.pusan_trip.dto.LoginRequestDto;
+import com.pusan_trip.dto.LoginResponseDto;
+import com.pusan_trip.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto) {
+        LoginResponseDto responseDto = userService.login(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
 }
--- backend/src/main/java/com/pusan_trip/dto/LoginRequestDto.java
+++ backend/src/main/java/com/pusan_trip/dto/LoginRequestDto.java
@@ -1,4 +1,11 @@
 package com.pusan_trip.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class LoginRequestDto {
+    private String userId;
+    private String password;
 }
